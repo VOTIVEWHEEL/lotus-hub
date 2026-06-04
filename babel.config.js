e90a8943d0716e7b9e13d55486c5adc3d17060db
VotiveWheel <163951580+VOTIVEWHEEL@users.noreply.github.com>
2026-06-04 02:08:01 +0100

    Create babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,25 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './src',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@store': './src/store',
+            '@hooks': './src/hooks',
+            '@utils': './src/utils',
+            '@types': './src/types',
+            '@constants': './src/constants',
+          },
+        },
+      ],
+    ],
+  };
+};
